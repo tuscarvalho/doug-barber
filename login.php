f7c98d99bd8e135f33e3d510a072b3841c3f029e
tuscarvalho <tiago.carvalho.sitel@gmail.com>
2017-05-14 20:13:05 -0300

    Area de Login

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Área de Login - Doug's Barber</title>
+	</head>
+	<body>
+	<header>
+		<p><h1>TESTE</h1></p>
+
+	</header>
+	</body>
+</html>
