748aeba018b58cb45550404d3b8ac5c3fdb24f55
tuscarvalho <tiago.carvalho.sitel@gmail.com>
2017-05-14 22:42:57 -0300

    Area de Login

--- login.php
+++ login.php
@@ -5,11 +5,25 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
 	</head>
 	<body>
-	<header>
-		
-
-	</header>
+		<header>
+			
+		</header>
+		<section class="container">
+			<form>
+				<div class="form-group">
+	    			<div class="col-md-3 col-md-offset-4">
+	    			<label class="col-md-6" for="telefone">Telefone:</label>	
+	    			<input class="form-control" type="text" name="telefone" id="telefone">
+			  	</div>
+			  	<div class="form-group">
+	    			<div class="col-md-3 col-md-offset-4">
+	    			<label class="col-md-6" for="telefone">Data de Nascimento:</label>	
+	    			<input class="form-control" type="data" name="data" id="data"></div>
+			  	</div>
+			</form>
+		</section>
 	</body>
 </html>
