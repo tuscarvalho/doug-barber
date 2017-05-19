14b1220c935425c6d9b39ed25339127c8978c876
tuscarvalho <tiago.carvalho.sitel@gmail.com>
2017-05-18 22:17:19 -0300

    Tela de Login

--- login.php
+++ login.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
-		<title>Área de Login - Doug's Barber</title>
+   		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+
+		<title>Área de Login - Doug's Barber</title>
+
+		/link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/estilo.css">
 	</head>
 	<body>
